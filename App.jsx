106103c49959053bae443ab9c9b53ce9a3873e2e
krishnanx <abhishekkrishnan2006@gmail.com>
2025-03-29 13:52:51 +0530

    universal navigation added

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import UniversalNavi from './Navigation/Universal';
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <UniversalNavi />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
