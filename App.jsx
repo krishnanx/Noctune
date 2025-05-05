ba0d08d16069dba330bfbbcc19e3bf4d64636bfb
krishnanx <abhishekkrishnan2006@gmail.com>
2025-05-05 18:49:32 +0530

    gave background color to splash screen

--- App.jsx
+++ App.jsx
@@ -35,14 +35,15 @@ export default function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      dispatch(loadUser()); //added this
-      setInterval(() => {
+      dispatch(loadUser());
+      setTimeout(() => {
         setStatus("idle");
-      }, 0);
+      }, 1000); // Wait 1 second then go idle
     };
 
     fetchData();
   }, []);
+
 
 
   if (status === "loading") {
