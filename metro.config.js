c6d186e4c0903a00d99ab128eba0620c261dbae1
krishnanx <abhishekkrishnan2006@gmail.com>
2025-05-16 21:27:29 +0530

    Metro config file updated to fix dev bug

--- metro.config.js
+++ metro.config.js
@@ -1,35 +1,36 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), {});
+const defaultConfig = getDefaultConfig(__dirname);
 
-// const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-// const path = require("path");
+const customConfig = {
+    resolver: {
+        unstable_conditionNames: ["browser", "require", "react-native"],
+    },
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+};
 
-// const config = {
-//   resolver: {
-//     unstable_conditionNames: ["browser", "require", "react-native"],
-//     assetExts: [
-//       ...require("metro-config/src/defaults/defaults").assetExts,
-//       "png",
-//       "jpg",
-//       "jpeg",
-//       "gif",
-//     ],
-//     extraNodeModules: {
-//       "missing-asset-registry-path": path.resolve(__dirname, "assets"),
-//     },
-//   },
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: true,
-//       },
-//     }),
-//   },
-// };
+// Manually merge defaultConfig and customConfig
+module.exports = {
+    ...defaultConfig,
+    ...customConfig,
+    resolver: {
+        ...defaultConfig.resolver,
+        ...customConfig.resolver,
+    },
+    transformer: {
+        ...defaultConfig.transformer,
+        ...customConfig.transformer,
+    },
+};
 
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
 
 // // metro.config.js
 // const { getDefaultConfig } = require("expo/metro-config");
