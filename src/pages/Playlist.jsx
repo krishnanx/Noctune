2198354be14408e05a902323429d1cce697847da
krishnanx <abhishekkrishnan2006@gmail.com>
2025-06-22 13:37:07 +0530

    artist name playlist bug fix

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -380,7 +380,7 @@ const Information = ({
       <View style={styles.metadata}>
         <View style={styles.imageContainer}>
           <Image
-            source={data.length > 0 ? data.image ? { uri: data.image } : { uri: data.songs[0].image } : icon}
+            source={data.songs.length > 0 ? data.image ? { uri: data.image } : { uri: data.songs[0].image } : icon}
             style={styles.albumArt}
           // fallback if user image fails to load
           />
@@ -467,7 +467,7 @@ const DataList = ({ styles, item }) => {
         <Text numberOfLines={1} ellipsizeMode="tail" style={styles.songName}>
           {item.title}
         </Text>
-        <Text style={styles.artistName}>{item.uploader}</Text>
+        <Text style={styles.artistName}>{item.uploader || item.artist}</Text>
       </View>
       <View style={styles.dotsContainer}>
         <ThreeDots />
