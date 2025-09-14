ba897358361014659d4c9f4815bda9c1171e28f7
krishnanx <abhishekkrishnan2006@gmail.com>
2025-09-14 18:48:47 +0530

    Mini player controls fixed

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -73,34 +73,20 @@ const Player = () => {
     };
   }, []);
   
-  const changePlayPause = async () => {
-    if (!soundRef.current) {
-      //console.warn("sound ref is null")
-      //console.warn(soundRef.current)
-      if (playRef.current) {
-        if (isplaying) {
-          //console.error("secomd")
-          await playRef.current.pauseAsync();
-          dispatch(progress(-1));
-        } else {
-          await playRef.current.playAsync(); // resumes from last position
-          dispatch(progress(-1));
-          //console.error("second")
-        }
-        dispatch(setIsPlaying("toggle"));
-        dispatch(setPlaylistplaying({ action: "toggle", id: playlistNo }));
-      }
+  const togglePlayPause = async () => {
+    console.warn("toggle")
+    console.warn(playbackState)
+    
+    if (playbackState.state === State.Playing || playbackState.state === State.Buffering) {
+      await TrackPlayer.pause();
     }
-    else if (isplaying) {
-      //console.warn("paused")
-      await soundRef.current.pauseAsync();
-      dispatch(progress(-1));
-      dispatch(setIsPlaying(false));
-    } else {
-      //console.warn("resumed")
-      await soundRef.current.playAsync(); // resumes from last position
-      dispatch(progress(-1));
-      dispatch(setIsPlaying(true));
+    else if(playbackState.state == State.Ended){
+      console.warn("ended state")
+      await TrackPlayer.seekTo(0)
+      await TrackPlayer.play()
+    } 
+    else {
+      await TrackPlayer.play();
     }
     
   };
@@ -110,42 +96,7 @@ const Player = () => {
     navigation.navigate('PlayerStack');
   };
 
-  const TOTAL_DURATION = canLoad? data ? data[pos]?.duration : 0 : song ? song[position]?.duration : 0
-
-  useEffect(() => {
-    togglePlayPauseRef.current = changePlayPause;
-    console.log("hola");
-  }, [changePlayPause]);
-
-  useEffect(() => {
-    let mediaListenersInitialized = false;
-    if (!mediaListenersInitialized) {
-      console.log("Setting up media notification listeners");
-
-
-      MediaNotificationManager.registerPlayPauseHandler(() => {
-        console.log("Play/Pause triggered from notification");
-        if (togglePlayPauseRef.current) {
-          togglePlayPauseRef.current();
-        } else {
-          //console.warn("togglePlayPauseRef is not available");
-        }
-      });
-
-      mediaListenersInitialized = true;
-      return () => {
-        console.log("Cleaning up media notification listeners");
-        MediaNotificationManager.removeAllListeners();
-        MediaNotificationManager.hideNotification();
-      };
-    }
-  }, []);
-
-  //Update notification when playback state changes
-  useEffect(() => {
-    MediaNotificationManager.updatePlaybackStatus(isplaying,seek);
-  }, [isplaying]);
-
+  const TOTAL_DURATION = currentTrack ? currentTrack.duration : 0
 
   const styles = StyleSheet.create({
     Main: {
@@ -227,7 +178,7 @@ const Player = () => {
     },
     miniProgressBar: {
       position: "absolute",
-      top: 54,
+      top: 47,
       left: 7,
       right: 3,
       height: 3,
@@ -476,7 +427,7 @@ const Player = () => {
 
             <View style={styles.miniPlayerControls}>
               <TouchableOpacity
-                onPress={changePlayPause}
+                onPress={togglePlayPause}
                 style={styles.miniPlayPauseButton}
               >
                 {playbackState.state == State.Playing || playbackState.state == State.Buffering ? (
@@ -496,7 +447,7 @@ const Player = () => {
                 style={[
                   styles.miniProgressBarFill,
                   {
-                    width: `${TOTAL_DURATION ? (seek / TOTAL_DURATION) * 100 : 0
+                    width: `${TOTAL_DURATION ? (progress.position / TOTAL_DURATION) * 100 : 0
                       }%`,
                   },
                 ]}
