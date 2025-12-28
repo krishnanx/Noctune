03a589c5180b161cf226845a00a44e85948fa941
krishnanx <abhishekkrishnan2006@gmail.com>
2025-12-28 23:46:11 +0530

    fix: mini player data issue

--- android/app/src/main/java/com/krishnanx/Noctune/MainActivity.kt
+++ android/app/src/main/java/com/krishnanx/Noctune/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ReactActivity() {
         handleShareIntent(intent)
     }
     
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent) // Update the activity's intent - this is crucial!
         
